bdd8855a7885b4571e607b7958e9e4a0605e98cc
SeanHolden <sean.holden@o2.com>
2013-06-25 10:27:41 +0100

    refactor of tests, added rvmrc file

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 describe Ticket do
   context "when creating new record" do
+
+    before { 2.times { create(:ticket) } }
+
+    subject { Ticket.last }
+
+    its(:ticket_no) { should eq Ticket.first.ticket_no+1 }
+
+    its(:status) { should eq "waiting" }
+
     it "creates record successfully if all fields are valid" do
-      expect { create(:ticket) }.to_not raise_error ActiveRecord::RecordInvalid
+      expect { create(:ticket) }.to change { Ticket.count }.by(1)
     end
 
     it "raises a RecordInvalid error if all fields are blank" do
@@ -21,16 +30,6 @@ describe Ticket do
     it "raises a RecordInvalid error if description field is blank" do
       expect { create(:ticket_blank_description) }.to raise_error ActiveRecord::RecordInvalid
     end
-  end
-
-  context "when new record is created" do
-    its(:status) { should eq "waiting" }
-  end
-
-  context "with creation of record, ticket_no is +1 of last ticket_no" do
-    before { (1..2).each { create(:ticket) } } # create two ticket records
-    subject { Ticket.last }
-    its(:ticket_no) { should eq Ticket.first.ticket_no+1 }
   end
 
 end
