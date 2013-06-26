57f25460e46c8d17f1b56a0373a29ef839a0b267
SeanHolden <sean.holden@o2.com>
2013-06-26 23:17:39 +0100

    can now create user via api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
 
     render json: users
   end
+
+  # POST /users
+  # POST /users.json
+  def create
+    new_user = User.new(params[:new_user])
+
+    if new_user.save
+      render json: {"notice"=>"new user created successfully."}
+    else
+      render json: {"alert"=>"user was not created. check your params."}
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,31 @@ describe UsersController do
     specify { body.sample.should include("id","name","msisdn") }
   end
 
+  describe "#create" do
+    context "with correct params" do
+      let(:request_to_create_user) do
+        post :create, {new_user:{name:"John",msisdn:"07712345678",group_id: 1}}, :format => :json
+      end
+
+      it "creates new user" do
+        expect{ request_to_create_user }.to change{ User.count }.by(1)
+      end
+
+      it "should give correct response in body" do
+        request_to_create_user
+        response.body.should include "new user created successfully."
+      end
+    end
+
+    context "without correct params" do
+      before { post :create, {}, :format => :json }
+
+      subject { response }
+
+      its(:body) { should include "user was not created. check your params." }
+    end
+  end
+
   after(:all) { User.destroy_all }
 
 end
