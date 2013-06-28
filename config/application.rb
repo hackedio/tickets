5bf134a41e0eebf712e6184beeab4ebfc30e892a
SeanHolden <sean.holden@o2.com>
2013-06-28 11:48:16 +0100

    attempting to fix precompile errors for heroku

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ end
 
 module Tickets
   class Application < Rails::Application
+    # fixing heroku precompile errors
+    config.assets.initialize_on_precompile=false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
